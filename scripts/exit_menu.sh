9527ff2fdb679458e8dd97cb3294d585ed3c26bb
Vineeth Kartha <vineeth.kartha@gmail.com>
2020-07-23 19:19:26 +0530

    Color themes of Rofi updated

--- scripts/exit_menu.sh
+++ scripts/exit_menu.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
 # Custom Rofi Script
+# nord theme
+polar_night1="#2E3440"
+polar_night2="#3B4252"
+polar_night3="#434C5E"
+polar_night4="#4C566A"
 
-BLACK="#000000"
-WHITE="#ffffff"
-RED="#e53935"
-GREEN="#43a047"
-YELLOW="#fdd835"
-BLUE="#1e88e5"
-MAGENTA="#00897b"
-CYAN="#00acc1"
-PINK="#d81b60"
-PURPLE="#8e24aa"
-INDIGO="#3949ab"
-TEAL="#00897b"
-LIME="#c0ca33"
-AMBER="#ffb300"
-ORANGE="#fb8c00"
-BROWN="#6d4c41"
-GREY="#757575"
-BLUE_GREY="#546e7a"
-DEEP_PURPLE="#5e35b1"
-DEEP_ORANGE="#f4511e"
-LIGHT_BLUE="#039be5"
-LIGHT_GREEN="#7cb342"
+snow_storm1="#D8DEE9"
+snow_storm2="#E5E9F0"
+snow_storm3="#ECEFF4"
 
-BORDER="#1F1F1F"
-SEPARATOR="#1F1F1F"
-FOREGROUND="#FFFFFF"
-BACKGROUND="#2f343f"
-BACKGROUND_ALT="#2f343f"
-HIGHLIGHT_BACKGROUND=$WHITE
-HIGHLIGHT_FOREGROUND=$BACKGROUND
+frost1="#8FBCBB"
+frost2="#88C0D0"
+frost3="#81A1C1"
+frost4="#5E81AC"
 
+aurora_red="#BF616A"
+aurora_amber="#D08770"
+aurora_yellow="#EBCB8B"
+aurora_green="#A3BE8C"
+aurora_pink="#B48EAD"
 
+BORDER=$polar_night1
+SEPARATOR=$snow_storm3
+FOREGROUND=$snow_storm1
+BACKGROUND=$polar_night1
+BACKGROUND_ALT=$polar_night2
+HIGHLIGHT_BACKGROUND=$frost4
+HIGHLIGHT_FOREGROUND=$aurora_yellow
 
 # Launch Rofi
-MENU="$(rofi -no-lazy-grab -sep "|" -dmenu -i -p 'System :' \
+MENU="$(rofi -no-lazy-grab -sep "|" -dmenu -i -p 'PowerOff' \
 -hide-scrollbar true \
 -bw 0 \
 -lines 4 \
@@ -47,15 +42,15 @@ MENU="$(rofi -no-lazy-grab -sep "|" -dmenu -i -p 'System :' \
 -location 3 \
 -columns 1 \
 -show-icons -icon-theme "Papirus" \
--font "Monospace 15" \
+-font "Monospace 18" \
 -color-enabled true \
 -color-window "$BACKGROUND,$BORDER,$SEPARATOR" \
 -color-normal "$BACKGROUND_ALT,$FOREGROUND,$BACKGROUND_ALT,$HIGHLIGHT_BACKGROUND,$HIGHLIGHT_FOREGROUND" \
--color-active "$BACKGROUND,$BLUE,$BACKGROUND_ALT,$HIGHLIGHT_BACKGROUND,$HIGHLIGHT_FOREGROUND" \
--color-urgent "$BACKGROUND,$YELLOW,$BACKGROUND_ALT,$HIGHLIGHT_BACKGROUND,$HIGHLIGHT_FOREGROUND" \
+-color-active "$BACKGROUND,$aurora_red,$BACKGROUND_ALT,$HIGHLIGHT_BACKGROUND,$HIGHLIGHT_FOREGROUND" \
+-color-urgent "$BACKGROUND,$aurora_yellow,$BACKGROUND_ALT,$HIGHLIGHT_BACKGROUND,$HIGHLIGHT_FOREGROUND" \
 <<< " Lock|  Logout|  Reboot|  Shutdown")"
 case "$MENU" in
-  *Lock) i3lock -c 000000 ;;
+  *Lock) betterlockscreen -l dim;;
   *Logout) i3-msg exit;;
   *Reboot) reboot ;;
   *Shutdown) shutdown now;;
