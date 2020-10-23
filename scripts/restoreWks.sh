44bb52ddecef5ae98899ae81ea03d137aeb1bb97
Vineeth Kartha <vineeth.kartha@gmail.com>
2020-10-23 13:41:08 +0530

    comment updates

--- scripts/restoreWks.sh
+++ scripts/restoreWks.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
+# This script requires i3-resurrect to to be installed
+# pip3 install --user --upgrade i3-resurrect
 # The workspace layouts were saved with
 # i3-resurrect save --swallow=class,instance -d .config/i3-resurrect/ -p 'wk1'
-# i3-resurrect save --swallow=class,instance -d .config/i3-resurrect/ -p 'wk2'
+# i3-resurrect save --swallow=class,instance -d .config/i3-resurrect/ -p 'wk3'
 
 
 i3-resurrect restore -p 'wk1' -d .config/i3-resurrect/ -w '1: '
